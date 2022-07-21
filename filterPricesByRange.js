ad6cd81a5e3a45942cec825f2d48a71dd1558608
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-07-21 22:49:15 +0300

    Initial

--- filterPricesByRange.js
+++ filterPricesByRange.js
@@ -1,41 +1,46 @@
 // Solution
+const allNum = (...numbers) => numbers.every(num => (typeof num === 'number'));
+
+const allAboveZero = (...numbers) => numbers.every(num => (num >= 0));
+
+const checkRequirePrice = (reqLess, reqGreat, priceLess, priceGreat) => {
+  if (!allNum(reqLess, reqGreat, priceLess, priceGreat))
+    throw new Error('Uncorrect type arg');
+  if (!allAboveZero(reqLess, reqGreat, priceLess, priceGreat))
+    throw new Error('Price less than zero');
+  if ((reqGreat < reqLess) || (priceGreat < priceLess))
+    throw new Error('Uncorrect range');
+}
+
 const check = (requireLess, requireGreat, priceLess, priceGreat) => {
-  if (!requireLess) requireLess = 0
-  if (!priceLess) priceLess = 0
-
-  if(!requireGreat && !priceGreat) return true
-  if (!requireGreat) return priceGreat >= requireLess
-  if (!priceGreat) return requireGreat >= priceLess
-
-  if (priceLess > requireGreat || priceGreat < requireLess) return false
-
-  return true
+  checkRequirePrice(requireLess, requireGreat, priceLess, priceGreat);
+  if ((priceLess > requireGreat) || (priceGreat < requireLess)) return false;
+  return true;
 }
 
-const filterByPrices = (less, great) => {
-  let results = []
-  for (let course of courses) {
-    if (check(less, great, ...course.prices)) results.push(course)
-  }
-  return results
-}
+const checkPrice = (less, great, course) => check(less, great, ...course.prices);
 
 // Task
-let courses = [
+const courses = [
   { name: "Courses in England", prices: [0, 100] },
-  { name: "Courses in Germany", prices: [500, null] },
+  { name: "Courses in Germany", prices: [500, Infinity] },
   { name: "Courses in Italy", prices: [100, 200] },
-  { name: "Courses in Russia", prices: [null, 400] },
+  { name: "Courses in Russia", prices: [0, 400] },
   { name: "Courses in China", prices: [50, 250] },
-  { name: "Courses in USA", prices: [200, null] },
+  { name: "Courses in USA", prices: [200, Infinity] },
   { name: "Courses in Kazakhstan", prices: [56, 324] },
-  { name: "Courses in France", prices: [null, null] },
+  { name: "Courses in France", prices: [0, Infinity] },
 ];
 
-let requiredRange1 = [null, 200];
-let requiredRange2 = [100, 350];
-let requiredRange3 = [200, null];
+const requiredRanges = [
+  [0, 200],
+  [100, 350],
+  [200, Infinity],
+];
 
-console.log(filterByPrices(...requiredRange1))
-console.log(filterByPrices(...requiredRange2))
-console.log(filterByPrices(...requiredRange3))
+for (const range of requiredRanges) {
+  console.log(range);
+  for (const course of courses) {
+      if (checkPrice(...range, course))console.log(course)
+  }
+}
